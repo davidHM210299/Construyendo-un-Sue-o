6e0a01d62187196f7a4f1e77d8864c9b44eb0eee
davidHM210299 <120129759+davidHM210299@users.noreply.github.com>
2022-12-08 17:52:28 -0600

    Add files via upload

---
+++ src/jOptionPane.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Toshiba
+ */
+class jOptionPane {
+
+    static void showMessageDialog(Object object, String algun_campo_esta_vacio) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
